a7a6abd5b6766c5c24df57e6e6527b6a23e56289
w <36293421+ppond454@users.noreply.github.com>
2022-03-01 10:38:46 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, jsonify, request, send_file
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
+import os
+import random
+import string
+
+
+app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = "file"
+app.config["MAX_CONTENT_LENGHT"] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(["csv"])
+
+CORS(app)
+
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({
+        "msg": "hello"
+    }), 200
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+
+    if request.method == "POST":
+
+        if "file" not in request.files:
+            return jsonify({
+                "massage" : "file is required"
+            }),400
+        
+        file = request.files['file']
+        if file.filename == "": 
+            return jsonify({
+                "massage":"No file selected for uploading"
+            }),400
+        if file and allowed_file(file.filename) :
+            randomStr = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(5))
+            filename = secure_filename(randomStr)
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"],filename+".csv"))
+            return jsonify({
+                "massage":"File uploaded successfully",
+                "id" : filename
+            }),201
+        else :
+            return jsonify({'massage': 'Allowed file types is csv'}), 400
+
+@app.route("/download/<path:filename>",methods=["GET"])
+def download(filename):
+    if request.method == "GET":
+        f = os.path.join(app.config["UPLOAD_FOLDER"],filename+".csv")
+        return  send_file(f,as_attachment=True),200
+
+
+if __name__ == "__main__":
+    app.run(port=4000,debug=True)
